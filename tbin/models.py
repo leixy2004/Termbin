efd1d4046e883298a7e4173b3cf1cd76ad723240
leixy2004 <leixy2004@gmail.com>
2023-10-03 22:50:32 +0800

    Finished basic part, but in gui.

--- tbin/models.py
+++ tbin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
+from django.urls import reverse
 # Create your models here.
+class Paste(models.Model):
+    paste_data = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    paste_digest = models.CharField(max_length=200)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4)
+    # def get_absolute_url(self):
+    #     return reverse("model_detail", kwargs={"pk": self.pk})
+    
