27419386c5c7f82b292aba0a56905372752cd201
leixy2004 <leixy2004@gmail.com>
2023-10-04 11:18:30 +0800

    Now submit func is finished

--- tbin/models.py
+++ tbin/models.py
@@ -4,11 +4,11 @@ import uuid
 from django.urls import reverse
 # Create your models here.
 class Paste(models.Model):
-    paste_data = models.TextField()
+    paste_text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     paste_digest = models.CharField(max_length=200)
     id = models.UUIDField(primary_key=True,default=uuid.uuid4)
     # def get_absolute_url(self):
     #     return reverse("model_detail", kwargs={"pk": self.pk})
     def __str__(self) -> str:
-        return self.paste_data
+        return self.paste_text
--- tbin/views.py
+++ tbin/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse,HttpRequest, Http404
 from .models import Paste
 # Create your views here.
-def index(request):
+def index(request : HttpRequest):
+    if request.method == "POST":
+        text = request.body
+        text=text[text.find(b'\r\n',text.find(b'\r\n',text.find(b'\r\n')+1)+1)
+                  :text.rfind(b'\r\n',0,text.rfind(b'\r\n')-1)]
+        text=text.strip(b'\r\n')
+        paste=Paste(paste_text=text.decode(),paste_digest=hash(text))
+        paste.save()
     return HttpResponse("Here is INDEX.")
 
 def detail(request,paste_uuid):
